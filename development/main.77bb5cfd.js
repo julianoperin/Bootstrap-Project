e59fd40ed8c29246be71004d1f6802fe7a0d7ded
‘Juliano <julianoperins@gmail.com>
2020-07-13 01:45:21 -0400

    working on the hero section

--- development/main.77bb5cfd.js
+++ development/main.77bb5cfd.js
@@ -184,7 +184,7 @@ function reloadCSS() {
 }
 
 module.exports = reloadCSS;
-},{"./bundle-url":"../../../../../usr/local/lib/node_modules/parcel-bundler/src/builtins/bundle-url.js"}],"css/main.scss":[function(require,module,exports) {
+},{"./bundle-url":"../../../../../usr/local/lib/node_modules/parcel-bundler/src/builtins/bundle-url.js"}],"scss/main.scss":[function(require,module,exports) {
 var reloadCSS = require('_css_loader');
 
 module.hot.dispose(reloadCSS);
@@ -394,4 +394,4 @@ function hmrAcceptRun(bundle, id) {
   }
 }
 },{}]},{},["../../../../../usr/local/lib/node_modules/parcel-bundler/src/builtins/hmr-runtime.js"], null)
-//# sourceMappingURL=/main.54166fbf.js.map
+//# sourceMappingURL=/main.77bb5cfd.js.map
--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,5 @@
 const sayHello = () => {
   console.log("HELOOO!!!");
 };
+
+sayHello();
